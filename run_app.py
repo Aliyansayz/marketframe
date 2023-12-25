a2c77e556564d0f92719b6eb493731ffd583733d
Aliyan Anwar <muhammad.aliyan22@gmail.com>
2023-12-25 17:39:57 +0500

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -13,6 +13,13 @@
 # print(bar_df)
                 """
 # ema_period     =  [ 5 , 20 ]
+from get_clean_data import  get_clean_data
+from resample_data  import  resample_data
+from indicators     import   access_indicators
+
+from indicator_lookback import indicators_lookback_mode
+from sorting  import  sorting 
+
 import yfinance as yf
 
 symbols_forex = [
