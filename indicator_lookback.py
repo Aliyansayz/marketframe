dd3c358829c376ec2d6ce05a5769c567df12a1da
Aliyan Anwar <muhammad.aliyan22@gmail.com>
2024-01-07 00:19:03 +0500

    Update indicator_lookback.py

--- indicator_lookback.py
+++ indicator_lookback.py
@@ -12,7 +12,7 @@ class  indicators_lookback_mode( access_indicators ):
 
     symbols   = 0
     values    = 1
-    indicator = cls() # universal class of project => adx_indicator
+    indicator = cls() # universal class of project => access_indicator
 
     heikin_ashi = indicator.get_heikin_ashi (bar_list = refine_list,  ohlc_data=True)
 
@@ -25,20 +25,20 @@ class  indicators_lookback_mode( access_indicators ):
 
     crossover_direction_list   =  indicator.crossover_direction_lookback(bar_list = refine_list, ema_period = ema_period ,lookback = lookback )
 
-    stochastic_momentum_list =  indicator.stochastic_momentum_lookback( bar_list = refine_list, period = ema_period[1], lookback = lookback, ema_period = 5 )
+    stochastic_momentum_list =  indicator.stochastic_momentum_lookback( bar_list = refine_list, period = ema_period[1], lookback = lookback, ema_period = 5)
 
-    stochastic_momentum_crossover_list = indicator.stochastic_momentum_lookback( bar_list = refine_list, period = ema_period[1], lookback = lookback, ema_period = 5, crossover_direction=True)
+    stochastic_momentum_crossover_list = indicator.stochastic_momentum_lookback( bar_list = refine_list, period = ema_period[1], lookback = lookback, ema_period = ema_period[0], crossover_direction=True)
     emaz_list  =  indicator.ema_lookback( bar_list = refine_list, lookback = lookback , ema_period = ema_period )
 
     atr_bands_list = indicator.atr_bands_lookback( refine_list , multiplier,  period = atr_period ,  lookback = lookback  )
     adx_value_list = indicator.adx_lookback( bar_list = refine_list,   period = adx_period , lookback = lookback)
 
-
-    bollinger_bands_list = indicator.get_bollinger_bands(  bar_list= refine_list, lookback = lookback )
+    channel_list, trend_outofchannel_list = indicator.get_linear_regression_channel_lookback(bar_list= refine_list , period=21, dev_multiplier=2.0, lookback = lookback )
+    bollinger_bands_list = indicator.get_bollinger_bands( bar_list= refine_list, lookback = lookback )
 
     # lookback = 10
     dt   = np.dtype([ ('index', 'datetime64[h]'),  ('symbol', 'U20'), ('Open', float ), ('High', float ), ('Low', float),  ('Close', float ),  ('Heikin-Ashi-Status', 'U10'),  ('Direction', float), ('Average-Directional-Index', float), ('Crossover', float),   \
-        ('Stop_Loss', float), ('Take_Profit', float), ('direction_smi', float), ('crossover_smi', float), ('ema_low', float), ('ema_high', float), ('smi', float), ('smi_ema', float), ('bb_lower', float ), ('bb_upper', float), ('atr_lower', float), ('atr_upper', float ), ('ha_open', float), ('ha_high', float),('ha_low', float),('ha_close', float)  ])
+        ('Stop_Loss', float), ('Take_Profit', float), ('direction_smi', float), ('crossover_smi', float), ('ema_low', float), ('ema_high', float), ('smi', float), ('smi_ema', float), ('', 'U10' ), ('trend_status', 'U10' ), ('outofchannel_status', 'U10' ), ('bb_lower', float ), ('bb_upper', float), ('atr_lower', float), ('atr_upper', float ), ('ha_open', float), ('ha_high', float),('ha_low', float),('ha_close', float)  ])
 
 
     column_names = dt.names
@@ -62,7 +62,10 @@ class  indicators_lookback_mode( access_indicators ):
             smi =     [ element[0] for element in stochastic_momentum_list[sym] ]
             smi_ema = [ element[1] for element in stochastic_momentum_list[sym] ]
 
-            direction_smi        = [ element[0] for element in stochastic_momentum_crossover_list[sym] ]
+            trend_status        = [ element[0] for element in trend_outofchannel_list[sym] ]
+            outofchannel_status = [ element[1] for element in trend_outofchannel_list[sym] ]
+
+            direction_smi = [ element[0] for element in stochastic_momentum_crossover_list[sym] ]
             crossover_smi = [ element[1] for element in stochastic_momentum_crossover_list[sym] ]
 
 
@@ -97,7 +100,7 @@ class  indicators_lookback_mode( access_indicators ):
                       take_profit = lower_band[i]
 
                 ohlc_df[i] = ( index[i], symbol[i], open[i], high[i], low[i], close[i], heikin_ashi_status[i], direction[i], adx_value[i], \
-                               crossover[i], stop_loss, take_profit, direction_smi[i] , crossover_smi[i],  short_ema[i], long_ema[i], smi[i], smi_ema[i],  bb_lower[i], bb_upper[i], lower_band[i], upper_band[i], ha_open[i], ha_high[i] , ha_low[i], ha_close[i]  )
+                               crossover[i], stop_loss, take_profit, direction_smi[i] , crossover_smi[i],  short_ema[i], long_ema[i], smi[i], smi_ema[i], trend_status[i], outofchannel_status[i], bb_lower[i], bb_upper[i], lower_band[i], upper_band[i], ha_open[i], ha_high[i] , ha_low[i], ha_close[i]  )
 #
 
             data[sym] = ohlc_df
